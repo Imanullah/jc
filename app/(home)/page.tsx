745ac930cc78147e77c3acbb31d517244d9fd629
IMAN <iman@iMac.local>
2025-04-08 02:21:53 +0700

    Initial

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -39,8 +39,8 @@ export default function HomePage() {
   }, []);
 
   return (
-    <div className="flex flex-col h-dvh md:h-fit justify-between p-[20px]">
-      <div>
+    <div className="flex flex-col h-[calc(100svh-50px)] md:h-fit justify-between p-[20px]">
+      <div className="">
         <HexagonImage />
         <div className="pb-[20px] pt-[40px] px-1">
           <SlideUp>
