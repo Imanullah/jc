49e45815e489148336fa8fe2cf9f347121ad677a
IMAN <iman@iMac.local>
2025-04-08 16:20:10 +0700

    Initial

--- app/(home)/form/2/page.tsx
+++ app/(home)/form/2/page.tsx
@@ -32,10 +32,12 @@ export default function StartingPage() {
       </div>
       <div className="flex-1 flex flex-col items-center gap-5 py-[20px]">
         <HexagonImageSmall />
-        <p className="text-[#FAFAFA] font-bagoss text-[19px] text-center">
-          Thanks <span className="capitalize">{fname ? fname : '__'}</span>! Now, it's time to get a reallity check
-        </p>
-        <p className="text-[#FAFAFA] text-[19px] text-center">That will take 2-3 minutes</p>
+        <div className="pb-16">
+          <p className="text-[#FAFAFA] font-bagoss text-[19px] text-center">
+            Thanks <span className="capitalize">{fname ? fname : '__'}</span>! Now, it's time to get a reallity check
+          </p>
+          <p className="text-[#FAFAFA] text-[19px] text-center">That will take 2-3 minutes</p>
+        </div>
       </div>
       <div className="pt-[20px] flex flex-col gap-5 ">
         <CheckButton onClick={() => console.log('Fisnish')} className="bg-primary-white text-black border-8 border-black/85 p-[13px]">
