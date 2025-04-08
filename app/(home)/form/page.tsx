9cdff4f3e0e8c0a05177bd1ffce4fb8600c9e74e
IMAN <iman@iMac.local>
2025-04-08 16:26:23 +0700

    Initial

--- app/(home)/form/page.tsx
+++ app/(home)/form/page.tsx
@@ -73,9 +73,9 @@ export default function FormPage() {
       <div>
         <AppHeader routeBack="/check" />
       </div>
-      <div className={cn('shrink flex flex-col items-center gap-5 py-[25px]', { 'flex-1': !isKeyboardVisible })}>
+      <div className={cn('shrink flex flex-col items-center gap-5 py-[20px]', { 'flex-1': !isKeyboardVisible })}>
         <HexagonImageSmall />
-        <p className="text-[#FAFAFA] font-bagoss text-[19px] text-center pb-16">Let's start with the basics. Type in your first name.</p>
+        <p className="text-[#FAFAFA] font-bagoss text-[19px] text-center pb-20">Let's start with the basics. Type in your first name.</p>
       </div>
       <div className="pt-[20px] flex flex-col gap-5 ">
         <form onSubmit={handleSubmit(onSubmit)}>
